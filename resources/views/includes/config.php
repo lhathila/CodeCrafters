066b8049dcd92fb898b7e14b984f0f1464e9b369
Svishal2 <lhathial@gmail.com>
2026-07-12 12:37:10 +0530

    Create config.php

---
+++ resources/views/includes/config.php
@@ -0,0 +1,14 @@
+<?php
+// DB & app configuration
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'transitops',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'name' => 'TransitOps',
+    ],
+];
